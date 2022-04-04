f50b69b32ccf8f47a01a8bbad69fdf9179b8f168
jeremypineda <pinedasandoval2002@gmail.com>
2022-04-04 00:23:52 -0600

    agregando tablas

--- resources/views/livewire/alumnos.blade.php
+++ resources/views/livewire/alumnos.blade.php
@@ -1,6 +1,37 @@
 <div>
     {{-- Stop trying to control. --}}
 
-    
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Edad</th>
+                <th>Sexo</th>
+                <th>Cum</th>
+            </tr>
+
+        </thead>
+        <tbody>
+        @foreach($alumnos as $alumno)
+            <tr>
+                <td>{{ $alumno->id }}</td>
+                <td>{{ $alumno->nombres }}</td>
+                <td>{{ $alumno->apellidos }}</td>
+                <td>{{ $alumno->edad }}</td>
+                <td>{{ $alumno->sexo }}</td>
+                <td>{{ $alumno->cum }} </td>
+            </tr>
+
+
+         @endforeach
+
+
+        </tbody>
+
+
+
+    </table>
 
 </div>
