5ed71d687f048cd7dcd6e973b92dc2aa0f23e2ee
Rodolfo <smis037117@ugb.edu.sv>
2022-04-04 23:43:48 -0600

    funcionamiento de botones y diseño

--- app/Http/Livewire/Alumnos.php
+++ app/Http/Livewire/Alumnos.php
@@ -7,11 +7,57 @@ use App\Models\Alumno;
 
 class Alumnos extends Component
 {
-    public $alumnos;
-
+    public $alumnos,$nombres,$apellidos,$edad,$sexo,$cum,$id_alumno;
+    public $modal = false;
     public function render()
     {
         $this->alumnos = Alumno::all();
         return view('livewire.alumnos');
+    }
+    public function crear()
+    {
+        $this->limpiarCampos();
+        $this->abrirModal();
+    }
+
+    public function abrirModal() {
+        $this->modal = true;
+    }
+    public function cerrarModal() {
+        $this->modal = false;
+    }
+    public function limpiarCampos(){
+        $this->descripcion = '';
+        $this->cantidad = '';
+        $this->id_alumno = '';
+    }
+    public function editar($id)
+    {
+        $alumnos = alumno::findOrFail($id);
+        $this->id_alumno = $id;
+        $this->descripcion = $alumnos->descripcion;
+        $this->cantidad = $alumnos->cantidad;
+        $this->abrirModal();
+    }
+
+    public function borrar($id)
+    {
+        alumno::find($id)->delete();
+        session()->flash('message', 'Registro eliminado correctamente');
+    }
+
+    public function guardar()
+    {
+        alumno::updateOrCreate(['id'=>$this->id_alumno],
+            [
+                'descripcion' => $this->descripcion,
+                'cantidad' => $this->cantidad
+            ]);
+         
+         session()->flash('message',
+            $this->id_alumno ? '¡Actualización exitosa!' : '¡Alta Exitosa!');
+         
+         $this->cerrarModal();
+         $this->limpiarCampos();
     }
 }
--- resources/views/livewire/alumnos.blade.php
+++ resources/views/livewire/alumnos.blade.php
@@ -1,27 +1,42 @@
-<div>
+<x-slot name="header">
+    <h1 class="text-gray-900">Alumnos</h1>
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+        
+
     {{-- Stop trying to control. --}}
+        <button wire:click="crear()" class="bg-blue-500 hover:bg-blue-600 text-black font-bold py-2 px-4">Crear</button>
+        @if($modal)
+            @include('livewire.crear')   
+        @endif    
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Nombres</th>
-                <th>Apellidos</th>
-                <th>Edad</th>
-                <th>Sexo</th>
-                <th>Cum</th>
+        <table class="table-fixed w-full">
+            <thead>
+            <tr class="bg-indigo-600 text-white">
+                <th class="px-4 py-2">Id</th>
+                <th class="px-4 py-2">Nombres</th>
+                <th class="px-4 py-2">Apellidos</th>
+                <th class="px-4 py-2">Edad</th>
+                <th class="px-4 py-2">Sexo</th>
+                <th class="px-4 py-2">Cum</th>
+                <th class="px-4 py-2">Accion 1</th>
+                <th class="px-4 py-2">Accion 2</th>
             </tr>
-
+ 
         </thead>
         <tbody>
         @foreach($alumnos as $alumno)
             <tr>
-                <td>{{ $alumno->id }}</td>
-                <td>{{ $alumno->nombres }}</td>
-                <td>{{ $alumno->apellidos }}</td>
-                <td>{{ $alumno->edad }}</td>
-                <td>{{ $alumno->sexo }}</td>
-                <td>{{ $alumno->cum }} </td>
+                <td class="border px-4 py-2">{{ $alumno->id }}</td>
+                <td class="border px-4 py-2">{{ $alumno->nombres }}</td>
+                <td class="border px-4 py-2">{{ $alumno->apellidos }}</td>
+                <td class="border px-4 py-2">{{ $alumno->edad }}</td>
+                <td class="border px-4 py-2">{{ $alumno->sexo }}</td>
+                <td class="border px-4 py-2">{{ $alumno->cum }} </td>
+                    <td><button wire:click="editar({{$alumno->id}})" class="bg-blue-500 hover:bg-blue-600 text-black font-bold py-2 px-4">Editar</button></td>
+                    <td><button wire:click="borrar({{$alumno->id}})" class="bg-red-500 hover:bg-red-700 text-black font-bold py-2 px-4">Borrar</button></td>
             </tr>
 
 
@@ -31,7 +46,7 @@
         </tbody>
 
 
-
-    </table>
-
+        </table>
+        </div>
+    </div>
 </div>
